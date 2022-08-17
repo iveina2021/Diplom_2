ba51b3e203d8387833f11a55c8b2929228146334
Anna Kolmogorova <iveina2021@gmail.com>
2022-08-17 17:18:43 +0200

    add random to email

--- src/main/java/diplom_api/helper/TestUtils.java
+++ src/main/java/diplom_api/helper/TestUtils.java
@@ -1,10 +1,12 @@
 package diplom_api.helper;
 
+import java.util.UUID;
+
 public class TestUtils {
 
     public static final String PASSWORD = "1999191";
     public static final String NAME = "TED";
-    public static String EMAIL = "iveina!!!!!!!!@mail.ru";
+    public static String EMAIL = UUID.randomUUID() + "iveina!!!!!!!!@mail.ru";
     public static String STELLAR_BURGERS_URL = "https://stellarburgers.nomoreparties.site";
 
     public static String prepareCreateUserRequest(String email, String password, String name) {
